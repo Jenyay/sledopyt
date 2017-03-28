db283d55726b7347493095d4f8f5a68ad53549d0
Jenyay <jenyay.ilin@gmail.com>
2017-03-28 22:46:42 +0300

    Now BuildingNumberActivity uses BuildingCalculator.

--- app/src/main/java/net/jenyay/sledopyt/BuildingNumberActivity.java
+++ app/src/main/java/net/jenyay/sledopyt/BuildingNumberActivity.java
@@ -2,14 +2,17 @@ package net.jenyay.sledopyt;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import net.jenyay.calculator.Calculator;
+import net.jenyay.buildingcalculator.BuildingCalculator;
 import net.jenyay.calculator.exceptions.CalculatorException;
 import net.jenyay.calculator.exceptions.FormatException;
+import net.jenyay.calculator.exceptions.UnknownVariableException;
 
 import java.util.HashMap;
 
@@ -18,8 +21,8 @@ import java.util.HashMap;
  */
 
 public class BuildingNumberActivity extends Activity {
-    private Button _calculateBtn;
     private EditText _equation;
+    private EditText _streetName;
     private TextView _result;
 
     @Override
@@ -28,24 +31,47 @@ public class BuildingNumberActivity extends Activity {
         setContentView(R.layout.building_number);
 
         _equation = (EditText)findViewById(R.id.equation);
+        _streetName = (EditText)findViewById(R.id.street_name);
+
         _result = (TextView)findViewById(R.id.building_result);
-        _calculateBtn = (Button)findViewById(R.id.calculate_button);
 
-        _calculateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                HashMap<String, Double> variables = new HashMap<>();
-                Calculator calculator = new Calculator(variables);
-                String equation = _equation.getText().toString();
-
-                try {
-                    Double result = calculator.calculate(equation);
-                    _result.setText(String.valueOf(result));
-                }
-                catch (CalculatorException e) {
-
-                }
-            }
-        });
+        _equation.addTextChangedListener(new TextChangeListener());
+        _streetName.addTextChangedListener(new TextChangeListener());
     }
+
+    class TextChangeListener implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String equation = _equation.getText().toString();
+            String streetName = _streetName.getText().toString();
+
+            BuildingCalculator calculator = new BuildingCalculator();
+
+            try {
+                Double result = calculator.calculate(equation, streetName);
+                _result.setText(String.valueOf(result));
+            }
+            catch (FormatException e) {
+                _result.setText(R.string.calculator_format_error);
+            }
+            catch (UnknownVariableException e) {
+                String error = getResources().getString(R.string.calculator_unknown_variable) +
+                        ": " + e.getVarName();
+                _result.setText(error);
+            }
+            catch (CalculatorException e) {
+
+            }
+        }
+    };
 }
